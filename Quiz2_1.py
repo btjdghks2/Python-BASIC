b7374ac5e4d529731f11b55d73fbc967603df71b
박성환 <btjdghks2@gmail.com>
2021-05-25 10:17:26 +0900

    문제 풀이 쓰다 말음

--- Quiz2_1.py
+++ Quiz2_1.py
@@ -1,15 +1,32 @@
-def Quiz2_1():
+def summary(*args):
+    total = sum(args)
+    return total, max(args), min(args), total / len(args)
+
+def q3():
+    total, maxval, minval, avg = summary(80,75,90,95,85)
+    print(total, maxval,minval, avg)
+
+def q4():
+    s = """
+    we encourage everyone to contribute to Python
+    If you still have questions after reviewing the material
+    in this 
+    """
+
+    # wjdwp
+    s = s.replace(",","").repalce(".","").replace("\n","").upper()
+    splits = s.split()
+    print("splits:", splits)
+
+    summary = {}        # 결과 dict
+    for word in splits:
+        if word in summary.key():   # 이미 키에 있으면
+            summary[word] += 1
+        else:   # 키가 없으면
+            summary[word] = 1
 
 
-    score = int(input("두과목 평균 몇점을 받았어?"))
 
 
-        message = ""
-        if score >= 60:
-                message = "합격!"
-        eles:
-                score >= 50
-                message = "불합격!"
-
-        print(message)
-
+if __name__ == "__main__":
+    q3()

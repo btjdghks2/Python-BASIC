0067663010a70b804da7b209164e3fbbd7d8c4e2
박성환 <btjdghks2@gmail.com>
2021-05-26 11:16:37 +0900

    쿼리 약간마무리

--- mysqlite/database.py
+++ mysqlite/database.py
@@ -12,7 +12,7 @@ class Database:
     # 접속 메서드
     def open(self, db):
         try:
-            ㄴelf.conn = sqlite3.connect(db)
+            self.conn = sqlite3.connect(db)
             self.cursor = self.conn.cursor()
         except sqlite3.Error as e:
             print("Database 접속 실패")
@@ -23,3 +23,25 @@ class Database:
             self.conn.commit() # 남아있는 I,U,D 쿼리를 커밋
             self.cursor.close()
             self.conn.close()
+
+    #__enter__ : with 와 함께 사용했을 때 호출되는 생명주기
+    def __enter__(self):
+        return self
+
+    # __exit__ : with 절이 끝났을때 호풀되는 생명주기
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+    def execute_select(self,sql, parameter=None):
+        if parameter is not None: # 파라미터가 있다
+            self.cursor.execute(sql, parameter)
+        else: #파라미터가 없다
+            self.cursor.execute(sql)
+
+        data = list(self.cursor.fetchall())
+        return data
+    # INSERT UPDATE DELETE 쿼리 수행메서드
+    def execute_cud(self, sql, parameter=None):
+        if parameter is not None: #파라미터가 있다
+            self.cursor.execute(sql, parameter)
